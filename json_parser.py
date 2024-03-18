1d6027dc5425eb34a9d3785b7860b2b4593519f1
Rohan Shanbhag <rohanshanbhag2001@gmail.com>
2024-03-18 12:45:14 -0500

    Added automated testing, and starting to refine lexer and parser functions

--- json_parser.py
+++ json_parser.py
@@ -1,3 +1,7 @@
+def parser(data):
+    with open(data,"r") as file:
+        print("Henlo")
+
 def lexer(data):
     with open(data,"r") as file:
         json = ""
@@ -14,41 +18,8 @@ def lexer(data):
         else:
             return False
 
-def test1():
-    invalidPath = "./tests/step1/invalid.json"
-    validPath = "./tests/step1/valid.json"
-
-    pass1 = not lexer(invalidPath)
-    pass2 = lexer(validPath)
-    if pass1:
-        print("> test1 invalid.json PASSED")
-    else:
-        print("> test1 invalid.json FAILED")
-    
-    print("-----------------------------")
-    # print("#############################")
-
-    if pass2:
-        print("> test2 valid.json PASSED")
-    else:
-        print("> test2 valid.json FAILED")
-    
-    print("-----------------------------")
-    if pass1 and pass2:
-        return 0
-    else:
-        return 1        
-
-def runTests():
-    print("TESTING")
-    print()
-    print("#############################")
-    print("STEP 1")
-    print("-----------------------------")
-    print(test1())
-    print("#############################")
-
 def main():
+    from testing import runTests
     runTests()
     # lexer(data)
 
---
+++ testing.py
@@ -0,0 +1,139 @@
+from json_parser import lexer, parser
+
+def test4():
+    invalidPath = "./tests/step4/invalid.json"
+    validPath = "./tests/step4/valid.json"
+    validPath2 = "./tests/step4/valid2.json"
+
+    pass1 = not lexer(invalidPath)
+    pass2 = lexer(validPath)
+    pass3 = lexer(validPath2)
+    if pass1:
+        print("> test1 invalid.json PASSED")
+    else:
+        print("> test1 invalid.json FAILED")
+    
+    print("-----------------------------")
+
+    if pass2:
+        print("> test2 valid.json PASSED")
+    else:
+        print("> test2 valid.json FAILED")
+    
+    print("-----------------------------")
+
+    if pass3:
+        print("> test3 valid.json PASSED")
+    else:
+        print("> test3 valid.json FAILED")
+
+    if pass1 and pass2 and pass3:
+        return 0
+    else:
+        return 1      
+
+def test3():
+    invalidPath = "./tests/step3/invalid.json"
+    validPath = "./tests/step3/valid.json"
+
+    pass1 = not lexer(invalidPath)
+    pass2 = lexer(validPath)
+    if pass1:
+        print("> test1 invalid.json PASSED")
+    else:
+        print("> test1 invalid.json FAILED")
+    
+    print("-----------------------------")
+    # print("#############################")
+
+    if pass2:
+        print("> test2 valid.json PASSED")
+    else:
+        print("> test2 valid.json FAILED")
+    
+    print("-----------------------------")
+    if pass1 and pass2:
+        return 0
+    else:
+        return 1      
+
+def test2():
+    # invalidPaths = ["./tests/step1/invalid.json", "./tests/step1/invalid.json"]
+    # validPaths = ["./tests/step1/valid.json","./tests/step1/valid2.json"]
+
+    invalidPath = "./tests/step2/invalid.json"
+    invalidPath2 = "./tests/step2/invalid2.json"
+    validPath = "./tests/step2/valid.json"
+    validPath2 = "./tests/step2/valid2.json"
+
+    pass1 = not lexer(invalidPath) or not parser(invalidPath)
+    pass2 = not lexer(invalidPath2) or not parser(invalidPath2)
+    pass3 = lexer(validPath) and parser(validPath)
+    pass4 = lexer(validPath2) and parser(validPath2)
+
+    invalidTests = [pass1,pass2]
+    validTests = [pass3,pass4]
+    res = True
+    i = 1
+    for test in invalidTests:
+        if test:
+            print(f"> test{i} invalid.json PASSED")
+        else:
+            print(f"> test{i} invalid.json FAILED")
+        print("-----------------------------")
+        i += 1
+        res = res and test
+    
+    for test in validTests:
+        if test:
+            print(f"> test{i} valid.json PASSED")
+        else:
+            print(f"> test{i} valid.json FAILED")
+        print("-----------------------------")
+        i += 1
+        res = res and test
+
+    if res:
+        return 0
+    else:
+        return 1   
+
+def test1():
+    invalidPath = "./tests/step1/invalid.json"
+    validPath = "./tests/step1/valid.json"
+
+    pass1 = not lexer(invalidPath)
+    pass2 = lexer(validPath)
+    if pass1:
+        print("> test1 invalid.json PASSED")
+    else:
+        print("> test1 invalid.json FAILED")
+    
+    print("-----------------------------")
+
+    if pass2:
+        print("> test2 valid.json PASSED")
+    else:
+        print("> test2 valid.json FAILED")
+    
+    print("-----------------------------")
+    if pass1 and pass2:
+        return 0
+    else:
+        return 1        
+
+def runTests():
+    test = {test1:'t1', test2:'t2', test3:'t3', test4:'t4'}
+
+    testNum = 1
+
+    print("TESTING")
+    for key, val in test.items():
+        print()
+        print("#############################")
+        print(f"STEP {testNum}")
+        print("-----------------------------")
+        key()
+        print("#############################")
+        print()
+        testNum += 1
